737ed83a7fc710562d2dc567f77c28f7ebc58d3c
Prateek Giri <praeekgiri999@gmail.com>
2024-02-01 12:09:21 +0530

    e-2

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
-
+import React from "react";
+import ReactDOM from "react-dom";
 const heading = React.createElement("h1" , {id: "heading"} , "Hello Form React !");
 const root  = ReactDOM.createRoot(document.getElementById("root"));
 
